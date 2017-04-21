a07b8dda0bd201e1b4f7d84acafc142d73b984ce
Angular CLI <angular-cli@angular.io>
2017-04-21 20:57:31 +0200

    chore: initial commit from @angular/cli

---
+++ e2e/app.e2e-spec.ts
@@ -0,0 +1,14 @@
+import { DropwizardHealthcheckMonitorPage } from './app.po';
+
+describe('dropwizard-healthcheck-monitor App', () => {
+  let page: DropwizardHealthcheckMonitorPage;
+
+  beforeEach(() => {
+    page = new DropwizardHealthcheckMonitorPage();
+  });
+
+  it('should display message saying app works', () => {
+    page.navigateTo();
+    expect(page.getParagraphText()).toEqual('app works!');
+  });
+});
